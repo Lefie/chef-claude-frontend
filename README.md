480bf956817230638b0b0bf9b493c2c6deb482f8
Lefie <qiwenruan11@gmail.com>
2025-04-16 13:13:09 -0400

    set up project aand add header

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# chef-claude-frontend
+### Chef Claude 
+This is a simple app made with react that uses AI to generate recipes based on ingredients
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+
+import chef_logo from "../assets/chef-claude-icon.png"
+export default function Header() {
+    return (
+        <>
+        <header>
+            <img src={chef_logo} />
+            <h1> Chef Claude </h1> 
+        </header>
+        </>
+    )
+}
